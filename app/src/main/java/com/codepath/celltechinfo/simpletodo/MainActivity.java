99f7ca0b820b86d5204fe66afac45ac0a60aa17e
CellTech Info <jocelync200@gmail.com>
2018-10-26 05:30:47 -0400

    -Fix App crash on API 17 and later

--- app/src/main/java/com/codepath/celltechinfo/simpletodo/MainActivity.java
+++ app/src/main/java/com/codepath/celltechinfo/simpletodo/MainActivity.java
@@ -21,12 +21,6 @@ import java.util.ArrayList;
         private ArrayAdapter<String> itemsAdapter;
         private ListView lvItems;
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.menu_main, menu);
-            return true;
-        }
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
